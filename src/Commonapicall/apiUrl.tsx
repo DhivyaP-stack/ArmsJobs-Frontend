94d7c98484c325e6acfd140c23850f2ebb5ae924
DhivyaP-stack <dhivya.p@psdigitise.com>
2025-05-05 13:22:30 +0530

    changed apiurl

--- src/Commonapicall/apiUrl.tsx
+++ src/Commonapicall/apiUrl.tsx
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 export const apiUrl = {
-  apiUrlConfig: "http://192.168.1.10:8000",
+  // apiUrlConfig: "http://192.168.1.10:8000",
+  apiUrlConfig: "https://armsjob.vercel.app",
   // apiUrlConfig: "http://192.168.43.150:8000", Motorola One Power
   // apiUrlConfig: "https://djangoapp-mindful-beautys-projects.vercel.app",
   //apiUrlConfig: "https://mbrestapi-f8cphtgaf7fjdyb0.westcentralus-01.azurewebsites.net",    // Azure
